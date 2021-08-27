dfecd31dca53886e666a80f74306410cb80f4a76
Omar Salama <31420520+omar-salama@users.noreply.github.com>
2021-08-27 20:50:24 +0200

    add navbar and minor styles

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/navbar/navbar.component';
 import { OrdersComponent } from './components/orders/orders.component';
 
 @NgModule({
-  declarations: [AppComponent, OrdersComponent],
+  declarations: [AppComponent, OrdersComponent, NavBarComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
