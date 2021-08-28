bed4ea5deee0ea71fbcae8606253690dd604c0a0
Omar Salama <31420520+omar-salama@users.noreply.github.com>
2021-08-28 20:29:06 +0200

    production ready

--- client/src/services/orders.service.ts
+++ client/src/services/orders.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +9,7 @@ export class OrdersService {
 
   constructor(private http: HttpClient) { }
 
-  private url = 'http://localhost:3000/order/'
-
   getAllOrders() {
-    return this.http.get(this.url)
+    return this.http.get(`${environment.backendUrl}/order`)
   }
 }
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomerModule } from './customer/customer.module';
 import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), CustomerModule, OrdersModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    CustomerModule,
+    OrdersModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'front-build'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
